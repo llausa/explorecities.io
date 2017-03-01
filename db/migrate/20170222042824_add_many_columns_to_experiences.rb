464b4ed685ad2e4b4d6a3b259417e7aa8618bbbd
Llausa <santiago.chamon@gmail.com>
2017-03-01 11:41:02 +1000

    exp desc order

--- db/migrate/20170222042824_add_many_columns_to_experiences.rb
+++ db/migrate/20170222042824_add_many_columns_to_experiences.rb
@@ -2,7 +2,7 @@ class AddManyColumnsToExperiences < ActiveRecord::Migration[5.0]
   def change
   	add_column :experiences, :created_by,	:string
     add_column :experiences, :category,		:integer  
-    add_column :experiences, :votes,		:integer 
+    add_column :experiences, :votes,		:integer, default: 0
     add_column :experiences, :rank, 		:integer 
   end
 end
